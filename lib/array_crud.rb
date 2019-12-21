e34ef1884ee4d1e612b9538c188e95f053c27413
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 15:11:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,43 +2,41 @@ def create_an_empty_array
   [ ]
 end
 
-
+# empty array 
 
 
 def create_an_array
   ["mike","Miwha", "Rachael", "YS"]
 end
 
-
+# create an array 
 
 
 def add_element_to_end_of_array(array, element)
 return array.push(element)
 end
-
 add_element_to_end_of_array(["mike","Miwha", "Rachael", "YS"],"new item")
 
 
-
+# add element to end of array 
 
 
 def add_element_to_start_of_array( array, element)
   return array.unshift(element)
 end
-
 add_element_to_start_of_array(["mike","Miwha", "Rachael", "YS"], "blah")
 
 
+# add element to the start of an array 
 
 
 
 def remove_element_from_end_of_array(array)
     return array.pop
 end
-
 remove_element_from_end_of_array(["mike","Miwha", "Rachael", "YS"])
 
-
+# remove element from the end of the array 
 
 
 
@@ -46,16 +44,15 @@ remove_element_from_end_of_array(["mike","Miwha", "Rachael", "YS"])
 def remove_element_from_start_of_array(array)
      return array.shift
 end
-
  remove_element_from_start_of_array(["mike","Miwha", "Rachael", "YS"])
  
- 
+# remove element from the start of the array 
  
  
  
 
 def retrieve_element_from_index(array, index_number)
-    return array.index_number
+    return array([index_number])
 end
 
 retrieve_element_from_index(["mike","Miwha", "Rachael", "YS"], [1])
