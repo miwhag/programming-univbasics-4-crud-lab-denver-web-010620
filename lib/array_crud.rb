970f80c343939b673feef34e512d3f5e9ef879fe
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 15:08:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,30 +1,68 @@
 def create_an_empty_array
-  
+  [ ]
 end
+
+
+
 
 def create_an_array
-  
+  ["mike","Miwha", "Rachael", "YS"]
 end
+
+
+
 
 def add_element_to_end_of_array(array, element)
-  
+return array.push(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+add_element_to_end_of_array(["mike","Miwha", "Rachael", "YS"],"new item")
+
+
+
+
+
+def add_element_to_start_of_array( array, element)
+  return array.unshift(element)
 end
+
+add_element_to_start_of_array(["mike","Miwha", "Rachael", "YS"], "blah")
+
+
+
+
 
 def remove_element_from_end_of_array(array)
-  
+    return array.pop
 end
+
+remove_element_from_end_of_array(["mike","Miwha", "Rachael", "YS"])
+
+
+
+
+
 
 def remove_element_from_start_of_array(array)
-  
+     return array.shift
 end
 
+ remove_element_from_start_of_array(["mike","Miwha", "Rachael", "YS"])
+ 
+ 
+ 
+ 
+ 
+
 def retrieve_element_from_index(array, index_number)
-  
+    return array.index_number
 end
+
+retrieve_element_from_index(["mike","Miwha", "Rachael", "YS"], [1])
+
+
+
+
 
 def retrieve_first_element_from_array(array)
   
