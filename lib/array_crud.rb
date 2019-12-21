157d6f4bdca301b3e193b64d83a785435c4b4462
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 15:56:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -75,8 +75,7 @@ retrieve_last_element_from_array(["cat", "dog", "rabbit"])
 #  retrieved the last emement using -1 
 
 def update_element_from_index(array, index_number, element)
-i = array[index_number]
-
+array[index_number] = element
 
 update_element_from_index(["cat", "dog", "rabbit"], 2, "harry pig")
 
